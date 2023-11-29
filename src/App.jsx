e555455dbe6a903b4949cd11700516e7cd6ccc19
Replit user <>
2023-11-29 07:26:45 +0000

    Implemented the game board

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,36 @@
+import { useState } from 'react'
 import './App.css'
 
-export default function Square() {
+function Square({ value, onSquareClick }) {
   return (
-    <div className="w-[120px] flex flex-wrap place-items-center"></div>
+    <button className="w-[35px] h-[35px] bg-slate-300 p-2 border border-black" onClick={onSquareClick}>{value}</button>
+  )
+}
+
+export default function Board() {
+  const [square, setSquare] = useState(Array(9).fill(null))
+  const [xIsNext, setXIsNext] = useState(true);
+  function handleClick(i) {
+    const nextSquare = square.slice()
+    if (xIsNext) {
+      nextSquare[i] = "X"
+    } else {
+      nextSquare[i] = "O"
+    }
+    setSquare(nextSquare)
+    setXIsNext(!xIsNext)
+  }
+  return (
+    <div className="w-[150px] flex flex-wrap p-5">
+      <Square value={square[0]} onSquareClick={() => handleClick(0)} />
+      <Square value={square[1]} onSquareClick={() => handleClick(1)} />
+      <Square value={square[2]} onSquareClick={() => handleClick(2)} />
+      <Square value={square[3]} onSquareClick={() => handleClick(3)} />
+      <Square value={square[4]} onSquareClick={() => handleClick(4)} />
+      <Square value={square[5]} onSquareClick={() => handleClick(5)} />
+      <Square value={square[6]} onSquareClick={() => handleClick(6)} />
+      <Square value={square[7]} onSquareClick={() => handleClick(7)} />
+      <Square value={square[8]} onSquareClick={() => handleClick(8)} />
+    </div>
   )
 }
